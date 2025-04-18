bb1395df7b52e97958453c871994157c93df2661
bielborgesc <gabrielborges565@gmail.com>
2025-04-18 18:51:41 -0300

    v1 da navbar sem responsividade

--- frontend/src/components/header/Hero.jsx
+++ frontend/src/components/header/Hero.jsx
@@ -8,22 +8,30 @@ const HeroLp = () => {
 
   return (
     <header className="bg-[#2D2D2D] h-[480px] w-full flex rounded-b-[32px]">
-      <div className="w-[860px] h-full pt-[24px] pb-[24px] pl-[80px] flex rounded-bl-[32px]">
+      <div className="w-[860px] h-full pt-[24px] pb-[24px] pl-[80px]">
+
         {/* Header*/}
-        <nav className="w-full flex gap-[6px] h-[24px] "> {/*h tem que ser de acordo com o tamanho interno, mudar dps*/}
+        <nav className="flex gap-[6px] h-[24px] "> {/*h tem que ser de acordo com o tamanho interno, mudar dps*/}
+
           {/* Logo */}
-          <img 
-            src={LogoMonks}
-            alt=".monks logo" 
-            className="w-[140px] h-[24px]"
-          />
-          {/* Categorias */}
-          <ul className="flex gap-6 text-[20px] font-normal leading-[100%] text-center text-[#EAE8E4]">
-            <li><a href="#">Categoria 1</a></li>
-            <li><a href="#">Categoria 2</a></li>
-            <li><a href="#">Categoria 3</a></li>
-            <li><a href="#">Categoria 4</a></li>
-          </ul>
+          <div className="w-[140px] h-[24px]">
+            <img 
+              src={LogoMonks}
+              alt=".monks logo" 
+              className="w-full h-full object-contain"
+            />
+          </div>
+
+          {/* Categorias flex justify-center*/}
+          <div className="w-[640px]">
+            <ul className="flex tracking-[0.0em] font[400] text-[20px] text-[#EAE8E4] helvetica-light justify-evenly text-center">
+              <li className="w-[141px] h-[24px] gap-[10px]"><a href="#">Categoria 1</a></li>
+              <li className="w-[141px] h-[24px] gap-[10px]"><a href="#">Categoria 2</a></li>
+              <li className="w-[141px] h-[24px] gap-[10px]"><a href="#">Categoria 3</a></li>
+              <li className="w-[141px] h-[24px] gap-[10px]"><a href="#">Categoria 4</a></li> 
+            </ul>
+          </div>
+
         </nav>
       </div>
 
